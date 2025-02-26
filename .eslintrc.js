98b29e66f60d7b1e4514d1e84744c8edcb8c9fe1
Max Soukhomlinov <maxs@intellectit.com.au>
2025-02-26 11:43:16 +1100

    Fix package.json prepublishOnly script and ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,7 +55,8 @@ module.exports = {
 				'n8n-nodes-base/node-param-fixed-collection-type-unsorted-items': 'off',
 				'n8n-nodes-base/node-param-options-type-unsorted-items': 'off',
 				'n8n-nodes-base/node-class-description-inputs-wrong-regular-node': 'off',
-				'n8n-nodes-base/node-class-description-outputs-wrong': ['error', ['main']]
+				'n8n-nodes-base/node-class-description-outputs-wrong': 'off',
+				'n8n-nodes-base/node-param-resource-with-plural-option': 'off'
 			},
 		},
 	],
